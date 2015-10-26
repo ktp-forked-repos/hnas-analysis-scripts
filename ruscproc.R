7904e2527af468d379c048aa5e7de8f7d435a8a5
jgoodie <john.goodman@gmail.com>
2015-10-26 14:02:37 -0700

    second commit

---
+++ ruscproc.R
@@ -0,0 +1,37 @@
+ruscproc <- function(filelist, users, metrics){
+        result <- data.frame(row.names = metrics)
+        for(i in 1:length(filelist)){
+                data <- read.csv(filelist[i],colClasses = "character")
+                for(x in 1:length(metrics)){
+                        result[x,i] <- round(mean(as.numeric(data[[metrics[x]]])), 3)
+                }
+        }
+        colnames(result) <- users
+        result
+}
+
+#files <- c("Baseline_900users.csv", "Baseline_1200users.csv", "Baseline_1800users.csv", "Baseline_2400users.csv", "Baseline_2997users.csv")
+files <- c("FileAudit_Baseline_900users.csv", "FileAudit_Baseline_1200users.csv", "FileAudit_Baseline_1800users.csv", "FileAudit_Baseline_2400users.csv", "FileAudit_Baseline_2997users.csv")
+users <- c("900 users", "1200 users", "1800 users", "2400 users", "3000 users")
+metrics <- c("SMB2.Ops.sec", "VSPfs00.Ops.sec", "VSPfs00.CIFS.Read.Resp.Time", "VSPfs00.CIFS.Write.Resp.Time", "MFB", "MMB", "NibRX.Busy", "NibTX.Busy", 
+             "NibTCP.Busy", "NICC.Busy", "FDP.Busy", "TDP.Busy", "FSA.Busy", "MBI.Busy", "WDIR.Busy", "WFILE.Busy", "WLOG.Busy", "OBJ.Busy", "SI.Busy", "Eth.RX", 
+             "Eth.TX", "FC.RX", "FC.TX", "Port1.Disk.Read", "Port1.Disk.Write", "Port2.Disk.Read", "Port2.Disk.Write", "Port3.Disk.Read", "Port3.Disk.Write",
+             "Port4.Disk.Read", "Port4.Disk.Write")
+
+out <- ruscproc(files, users, metrics)
+write.csv(out, file = "FileAudit_Baseline_summary.csv")
+
+
+##########################
+# Notes
+##########################
+# data <- read.csv(filename, colClasses = "character")
+# files <- c("Baseline_900users.csv", "Baseline_1200users.csv", "Baseline_1800users.csv", "Baseline_2400users.csv", "Baseline_2997users.csv")
+# users <- c("900", "1200", "1800", "2400", "3000")
+# metrics <-colnames(data)
+# metrics <- c("SMB2.Ops.sec", "VSPfs00.Ops.sec", "VSPfs00.CIFS.Read.Resp.Time", "VSPfs00.CIFS.Write.Resp.Time", "MFB", "MMB", "NibRX.Busy", "NibTX.Busy", 
+#              "NibTCP.Busy", "NICC.Busy", "FDP.Busy", "TDP.Busy", "FSA.Busy", "MBI.Busy", "WDIR.Busy", "WFILE.Busy", "WLOG.Busy", "OBJ.Busy", "SI.Busy", "Eth.RX", 
+#              "Eth.TX", "FC.RX", "FC.TX", "Port1.Disk.Read", "Port1.Disk.Write", "Port2.Disk.Read", "Port2.Disk.Write", "Port3.Disk.Read", "Port3.Disk.Write",
+#              "Port4.Disk.Read", "Port4.Disk.Write")
+#
+# ruscproc(files, users, metrics)
